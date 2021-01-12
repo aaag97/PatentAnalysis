e39fb4df4b5cca930916863be762131022b1c086
Andre Ghattas <andre.ghattas@epfl.ch>
2021-01-12 11:58:04 +0100

    lots of progress

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
 # PatentAnalysis
-
-The idea of the project is as follows:
-
-* Conduct OCR on a corpus of US and GB patents (using services and API's such as ABBY ocrsdk.com, tesseract after layout analysis or other)
-* Analayze the claims area of the patents
--- ocrutils.py
+++ ocrutils.py
@@ -17,22 +17,30 @@ import random
 from PIL import Image
 
 
-def OCR_GB_patent(patent_pdf_path_index, test_list):
+def OCR_Pages(pages):
     patent_str = ''
-    patent_pdf_path = test_list[patent_pdf_path_index]
+    
+    for page_index in range(len(pages)):
+        str_from_img = pytesseract.image_to_string(pages[page_index], config='--psm 1')
+        patent_str = '{}{}'.format(patent_str, str_from_img)
+    return patent_str
+
+def OCR_GB_patent(patent_pdf_path_index, patent_list, dest="/Volumes/Non-Backup_Files/GB-patents/MachineReadableBaseline/"):
+    patent_str = ''
+    patent_pdf_path = patent_list[patent_pdf_path_index]
     patent_name = patent_pdf_path.split('/')[-1][:-4]
     imgs = convert_from_path(patent_pdf_path)
     for img_index in range(len(imgs)):
         img = imgs[img_index]
         str_from_img = pytesseract.image_to_string(img)
         patent_str = '{}\n{}'.format(patent_str, str_from_img)
-    with open('/Volumes/Non-Backup_Files/GB-patents/MachineReadableBaseline/{}.txt'.format(patent_name), "w") as text_file:
+    with open('{}{}.txt'.format(dest, patent_name), "w") as text_file:
         text_file.write("%s" % patent_str)
     if(patent_pdf_path_index % 1000 == 0):
         print('finished {}'.format(patent_pdf_path_index))
         
         
-def OCR_US_patent(patent_nb_index, patent_dict, patent_list):
+def OCR_US_patent(patent_nb_index, patent_dict, patent_list, dest="/Volumes/Non-Backup_Files/US-patents/MachineReadableBaseline/"):
     patent_str = ''
     patent_nb = patent_list[patent_nb_index]
     
@@ -41,5 +49,5 @@ def OCR_US_patent(patent_nb_index, patent_dict, patent_list):
         str_from_img = pytesseract.image_to_string(img)
         patent_str = '{}\n{}'.format(patent_str, str_from_img)
         
-    with open('/Volumes/Non-Backup_Files/US-patents/MachineReadableBaseline/{}.txt'.format(patent_nb), "w") as text_file:
+    with open('{}/{}.txt'.format(dest, patent_nb), "w") as text_file:
         text_file.write("%s" % patent_str)
